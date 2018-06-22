f0232c8e87b042ca9831ecf7e61c990466d75d44
circuit-specialists <jake@circuitspecialists.com>
2018-06-21 17:24:08 -0700

    initial

--- Smart_Home_Relay.ino
+++ Smart_Home_Relay.ino
@@ -1,15 +1,16 @@
 /*
- * written by Jake Pring from Circuit Specialists.com
- * licensed as GPLv3
+   written by Jake Pring from Circuit Specialists.com
+   licensed as GPLv3
   commands:
-    A00101A1 closes relay
-    A00101A2 opens relay
- */
-
+    0xA00100A1 opens relay
+    0xA00101A2 closes relay
+*/
 #include <ESP8266WiFi.h>
 
-const char* ssid = "circuitspecialsts.com";
-const char* password = "C!rCu!t!";
+const char* ssid = "";
+const char* password = "";
+byte close_relay[] = {0xA0,0x01,0x01,0xA2,0x0D,0x0A};
+byte open_relay[] = {0xA0,0x01,0x00,0xA1,0x0D,0x0A};
 
 // Create an instance of the server
 // specify the port to listen on as an argument
@@ -19,25 +20,22 @@ void setup() {
   Serial.begin(9600);
   delay(10);
 
-  // prepare GPIO2
-  pinMode(2, OUTPUT);
-  
   // Connect to WiFi network
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
   Serial.println("");
   Serial.println("WiFi connected");
-  
+
   // Start the server
   server.begin();
   Serial.println("Server started");
@@ -52,37 +50,37 @@ void loop() {
   if (!client) {
     return;
   }
-  
+
   // Wait until the client sends some data
   Serial.println("new client");
-  while(!client.available()){
+  while (!client.available()) {
     delay(1);
   }
-  
+
   // Read the first line of the request
   String req = client.readStringUntil('\r');
   Serial.println(req);
   client.flush();
-  
+
   // sort http request
   int val;
-  if (req.indexOf("/turnoff") != -1)
+  if (req.indexOf("/turnon") != -1) {
     val = 0;
-    Serial.write(A00101A2);
-  else if (req.indexOf("/turnon") != -1)
+    Serial.write(close_relay, sizeof(close_relay));
+  }
+  else if (req.indexOf("/turnoff") != -1) {
     val = 1;
-    Serial.write(A00101A1);
+    Serial.write(open_relay, sizeof(open_relay));
+  }
   else {
-    Serial.println("invalid request");
     client.stop();
     return;
   }
-  
   client.flush();
 
   // Prepare the response
   String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nRelay has been set to the ";
-  s += (val)?"on":"off";
+  s += (val) ? "off" : "on";
   s += "state\n</html>\n";
 
   // Send the response to the client
@@ -90,7 +88,7 @@ void loop() {
   delay(1);
   Serial.println("Client disonnected");
 
-  // The client will actually be disconnected 
+  // The client will actually be disconnected
   // when the function returns and 'client' object is detroyed
 }
 
