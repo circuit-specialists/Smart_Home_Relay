0a409d031642835092105791f48a25e4197fd6af
circuit-specialists <jake@circuitspecialists.com>
2018-07-16 13:54:30 -0700

    Major Update
    Added /Garagedoor, and OTA

--- Smart_Home_Relay.ino
+++ Smart_Home_Relay.ino
@@ -1,25 +1,27 @@
 /*
-   written by Jake Pring from Circuit Specialists.com
+   written by Jake Pring from CircuitSpecialists.com
    licensed as GPLv3
   commands:
     0xA00100A1 opens relay
     0xA00101A2 closes relay
 */
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 const char* ssid = "";
 const char* password = "";
 byte close_relay[] = {0xA0, 0x01, 0x01, 0xA2, 0x0D, 0x0A};
 byte open_relay[] = {0xA0, 0x01, 0x00, 0xA1, 0x0D, 0x0A};
-const char* use_static_up = "no";
+bool use_static_ip = false;
 IPAddress static_address(192, 168, 1, 248);
 IPAddress gateway_ip(192, 168, 1, 1);
 IPAddress subnet_mask(255, 255, 255, 0);
 IPAddress dns_one(8, 8, 8, 8);
 IPAddress dns_two(8, 8, 4, 4);
 
-// Create an instance of the server
-// specify the port to listen on as an argument
+// Create an instance of the server with port
 WiFiServer server(80);
 
 void setup() {
@@ -27,35 +29,78 @@ void setup() {
   delay(10);
 
   // Connect to WiFi network
-  Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
-
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(3000);
+    ESP.restart();
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
 
-  // Start the server
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("my_esp8266");
+  //ArduinoOTA.setPassword("admin");
+  // MD5 hash
+  //ArduinoOTA.setPasswordHash("456b7016a916a4b178dd72b947c152b7");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    switch (error) {
+      case OTA_AUTH_ERROR:
+        Serial.println("Auth Failed");
+        break;
+      case OTA_BEGIN_ERROR:
+        Serial.println("Begin Failed");
+        break;
+      case OTA_CONNECT_ERROR:
+        Serial.println("Connect Failed");
+        break;
+      case OTA_RECEIVE_ERROR:
+        Serial.println("Receive Failed");
+        break;
+      case OTA_END_ERROR:
+        Serial.println("End Failed");
+        break;
+    }
+  });
+  // Start the OTA server
+  ArduinoOTA.begin();
+  Serial.println("OTA Started... Ready");
+
+  // Start the http server
   server.begin();
   Serial.println("Server started");
 
   // Print the IP address
   Serial.println(WiFi.localIP());
   delay(100);
-  if (use_static_up == "yes") {
+  if (use_static_ip) {
     WiFi.config(static_address, gateway_ip, subnet_mask, dns_one, dns_two);
+    Serial.println(WiFi.localIP());
   }
-  Serial.println(WiFi.localIP());
 }
 
 void loop() {
+  ArduinoOTA.handle();
   // Check if a client has connected
   WiFiClient client = server.available();
   if (!client) {
@@ -75,13 +120,22 @@ void loop() {
 
   // sort http request
   int val;
+  String _mode = "";
   if (req.indexOf("/turnon") != -1) {
     val = 0;
     Serial.write(close_relay, sizeof(close_relay));
+    _mode = "Relay has been set to the on position\n</html>\n";
   }
   else if (req.indexOf("/turnoff") != -1) {
     val = 1;
     Serial.write(open_relay, sizeof(open_relay));
+    _mode = "Relay has been set to the off position\n</html>\n";
+  }
+  else if (req.indexOf("/garagedoor") != -1) {
+    Serial.write(open_relay, sizeof(open_relay));
+    delay(50);
+    Serial.write(close_relay, sizeof(close_relay));
+    _mode = "Garagedoor cycled";
   }
   else {
     client.stop();
@@ -90,9 +144,7 @@ void loop() {
   client.flush();
 
   // Prepare the response
-  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nRelay has been set to the ";
-  s += (val) ? "off" : "on";
-  s += "state\n</html>\n";
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n" + _mode;
 
   // Send the response to the client
   client.print(s);
